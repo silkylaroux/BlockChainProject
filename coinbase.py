1376ddc4b1deea4f226c8fbe2e71083a9003f605
bjwellsi <31425792+bjwellsi@users.noreply.github.com>
2019-05-01 17:35:24 -0600

    Update coinbase.py
    I've included two different spreads. For some reason there are two different ways to look up the exchange rate of a currency, and they seem to update at different rates. The exchange rate from ETH to USD is also identical to the rate from (USD to ETH)/1. However, the descriptions for buying and selling ETH from USD say that there's an included 1% fee, so they come up with different results. As a result I added a 1% fee to the exchange rate method.

--- coinbase.py
+++ coinbase.py
@@ -1,7 +1,8 @@
 import numpy as np
 import requests as req
 import json
-stable_coin = 'USD'
+stable_coin = 'USDC'
+other_coin = 'ETH'
 
 
 def get_name():
@@ -9,24 +10,40 @@ def get_name():
 
 
 # This returns the ask/bid spread of the coin as it relates to the stable coin of this exchange
-def get_spread(coin1, coin2=stable_coin):
+def get_spread(coin1=other_coin, coin2=stable_coin):
     spread = {
         'ask': 1,
         'bid': 1,
     }
     if coin1 == stable_coin:
         return spread
-    r2 = req.get("https://api.coinbase.com/v2/prices/" + coin1 + "-" + coin2 +"/buy")
-    r = req.get("https://api.coinbase.com/v2/prices/" + coin1 + "-" + coin2 +"/sell")
+    r2 = req.get("https://api.coinbase.com/v2/exchange-rates?currency=" + coin1)
+    r = req.get("https://api.coinbase.com/v2/exchange-rates?currency=" + coin2)
     pair = json.loads(r.text)
     #print(pair)
-    ask = pair['data']['amount']
+    ask = 1/(float(pair['data']['rates'][coin1]))*0.99
     spread['ask'] = ask
     pair = json.loads(r2.text)
     #print(pair)
-    bid = pair['data']['amount']
+    bid = float(pair['data']['rates'][coin2])*1.01
     spread['bid'] = bid
-    #print(coin1, spread)
+    print(coin1, spread)
+    return spread
+
+#gives the buy/sell price from USD, as opposed to exchange rates
+def alternate_spread(coin=other_coin):
+    spread = {}
+    r2 = req.get("https://api.coinbase.com/v2/prices/" + coin + "-USD/buy")
+    r = req.get("https://api.coinbase.com/v2/prices/" + coin + "-USD/sell")
+    pair = json.loads(r.text)
+    #print(pair)
+    ask = float(pair['data']['amount'])
+    spread['ask'] = ask
+    pair = json.loads(r2.text)
+    #print(pair)
+    bid = float(pair['data']['amount'])
+    spread['bid'] = bid
+    print(coin, spread)
     return spread
 
 
